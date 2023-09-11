2292c7472066f8a3e81d6fed403ae1befefb96cd
bearwithbeer <144028286+bearwithbeer@users.noreply.github.com>
2023-09-11 16:44:17 +0200

    Bjorns gren

--- gitOevelse.java
+++ gitOevelse.java
@@ -1,3 +1,9 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
 public class gitOevelse {
 
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
 }
